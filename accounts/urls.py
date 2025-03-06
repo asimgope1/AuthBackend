64642931d5b8ea50e69af4a6fdcd07f3208d754f
asimgope1 <asimgope53@gmail.com>
2025-03-06 19:00:48 +0530

    default

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import RegisterView, LoginView
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({"message": "Welcome to Auth Backend"})
 
 urlpatterns = [
+    path('', home),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
 ]
